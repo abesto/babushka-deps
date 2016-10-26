850c56e545cc190e7e16e9ed9ae3bab8ed98726d
Zoltan Nagy <abesto@abesto.net>
2016-10-26 21:26:09 +0200

    Install `ag`

--- all.rb
+++ all.rb
@@ -1,12 +1,15 @@
 dep 'all' do
 	requires 'VirtualBox Guest Additions',
-		'lightdm', 'xmonad', 'xfce', 'xclip.bin',
-		'xterm', 'urxvt', 'java',
+		'lightdm', 'xmonad', 'xfce', 'xclip.bin', 'xterm', 'urxvt',
+		'ssh client', 'vim', 'fish', 'ag.bin',
+	       	'java',
 		'symlink dotfile'.with('.gitconfig'),
-		'ssh client', 'vim', 'fish',
 	       	'firefox',
 		'spell checking',
 		'abesto user', 'colemak'
 end
 
 dep 'xclip.bin'
+dep 'ag.bin' do
+	installs 'the_silver_searcher'
+end
