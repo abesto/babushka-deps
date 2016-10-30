4fb735bf24e411d980404194c16fb52318340708
Zoltan Nagy <abesto@abesto.net>
2016-10-30 22:10:30 +0100

    dmenu, xmobar for xmonad

--- xmonad.rb
+++ xmonad.rb
@@ -1,7 +1,11 @@
-dep 'xmonad.bin'
-dep 'xmonad-contrib.lib'
-
 dep 'xmonad' do
   requires 'xmonad.bin', 'xmonad-contrib.lib',
-           'symlink dotfile'.with('.xmonad')
+           'dmenu.bin', 'xmobar.bin',
+           'symlink dotfile'.with('.xmonad'),
+           'symlink dotfile'.with('.config/xmobar/xmobarrc')
 end
+
+dep 'xmonad.bin'
+dep 'xmonad-contrib.lib'
+dep 'dmenu.bin'
+dep 'xmobar.bin'
